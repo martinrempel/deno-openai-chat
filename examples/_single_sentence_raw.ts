5ad6f872518a876793a04759584219508bbadfe3
martinrempel <github@martinrempel.de>
2023-05-25 22:28:50 +0200

    added example which logs the whole api response

---
+++ examples/_single_sentence_raw.ts
@@ -0,0 +1,20 @@
+import OpenAIChat from "../mod.ts";
+
+const API_KEY = Deno.env.get("OPENAI_API_KEY");
+if (!API_KEY) {
+  throw new Error("OPENAI_API_KEY is required.");
+}
+
+console.log(`Prompt: "Explain Deno in a single sentence."\n`);
+
+const completion = await new OpenAIChat(API_KEY).complete({
+  model: "gpt-3.5-turbo",
+  messages: [
+    {
+      role: "user",
+      content: "Explain Deno in a single sentence.",
+    },
+  ],
+});
+
+console.log(completion);
