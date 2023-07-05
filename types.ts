019e15e714660c358a49c9c6e6ff500a2e41bc5a
Martin Rempel <github@martinrempel.de>
2023-07-05 13:55:03 +0200

    updated list of available models

--- types.ts
+++ types.ts
@@ -1,11 +1,13 @@
 /** See https://platform.openai.com/docs/models/model-endpoint-compatibility */
 export type Model =
   | "gpt-4"
-  | "gpt-4-0314"
+  | "gpt-4-0613"
   | "gpt-4-32k"
-  | "gpt-4-32k-0314"
+  | "gpt-4-32k-0613"
   | "gpt-3.5-turbo"
-  | "gpt-3.5-turbo-0301";
+  | "gpt-3.5-turbo-0613"
+  | "gpt-3.5-turbo-16k"
+  | "gpt-3.5-turbo-16k-0613";
 
 export type Role = "user" | "assistant" | "system";
 export type Message = {
